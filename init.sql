99f4b8bc9c4a1428a3cd77d6d29c79150ecf0bbb
Gabriel Victor <gabriel.silva@fiap.com.br>
2024-08-17 14:06:52 -0300

    arrumei para que o init.sql funcionasse também no python

--- init.sql
+++ init.sql
@@ -2,53 +2,53 @@ IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'Banco_Dados')
 BEGIN
     CREATE DATABASE Banco_Dados;
 END
-GO
-USE Banco_Dados
-GO
+
+USE Banco_Dados;
+
 IF EXISTS(SELECT * FROM SysObjects WHERE xtype='U' AND name = 'Notas')
 BEGIN
-	DROP TABLE Notas
+    DROP TABLE Notas;
 END
-GO
+
 IF EXISTS(SELECT * FROM SysObjects WHERE xtype='U' AND name = 'Ranking')
 BEGIN
-	DROP TABLE Ranking
+    DROP TABLE Ranking;
 END
-GO
+
 CREATE TABLE Notas (
-	Codigo INT NOT NULL IDENTITY,
-	RM INT NOT NULL,
-	Nome VARCHAR(255) NOT NULL,
-	Ano INT NOT NULL,
-	Semestre INT NOT NULL,
-	Turma VARCHAR(10) NOT NULL,
-	CodigoCurso INT NOT NULL,
-	NomeCurso VARCHAR(255) NOT NULL,
-	CodigoDisciplina INT NOT NULL,
-	Disciplina VARCHAR(255) NOT NULL,
-	CH INT NOT NULL,
-	Nac1 FLOAT,
-	PS1 FLOAT,
-	Media1 FLOAT,
-	Nac2 FLOAT,
-	PS2 FLOAT,
-	Media2 FLOAT,
-	MediaParcial FLOAT,
-	NotaExame FLOAT,
-	MediaFinal FLOAT,
-	Resultado VARCHAR(30)
-)
-GO
+    Codigo INT NOT NULL IDENTITY,
+    RM INT NOT NULL,
+    Nome VARCHAR(255) NOT NULL,
+    Ano INT NOT NULL,
+    Semestre INT NOT NULL,
+    Turma VARCHAR(10) NOT NULL,
+    CodigoCurso INT NOT NULL,
+    NomeCurso VARCHAR(255) NOT NULL,
+    CodigoDisciplina INT NOT NULL,
+    Disciplina VARCHAR(255) NOT NULL,
+    CH INT NOT NULL,
+    Nac1 FLOAT,
+    PS1 FLOAT,
+    Media1 FLOAT,
+    Nac2 FLOAT,
+    PS2 FLOAT,
+    Media2 FLOAT,
+    MediaParcial FLOAT,
+    NotaExame FLOAT,
+    MediaFinal FLOAT,
+    Resultado VARCHAR(30)
+);
+
 CREATE TABLE Ranking (
-	Codigo INT NOT NULL IDENTITY,
-	RM INT NOT NULL,
-	Nome VARCHAR(255) NOT NULL,
-	Ano INT NOT NULL,
-	Semestre INT NOT NULL,
-	Turma VARCHAR(10) NOT NULL,
-	QtdeDisciplina INT,
-	MediaBolsaMerito FLOAT,
-	PosicaoBolsaMerito INT,
-	PorcentagemBolsaMerito FLOAT,
-	MediaAcessoAlura FLOAT
-)
+    Codigo INT NOT NULL IDENTITY,
+    RM INT NOT NULL,
+    Nome VARCHAR(255) NOT NULL,
+    Ano INT NOT NULL,
+    Semestre INT NOT NULL,
+    Turma VARCHAR(10) NOT NULL,
+    QtdeDisciplina INT,
+    MediaBolsaMerito FLOAT,
+    PosicaoBolsaMerito INT,
+    PorcentagemBolsaMerito FLOAT,
+    MediaAcessoAlura FLOAT
+);
