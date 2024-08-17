72d8968d778b825a71414e551f0a39453ff4f3cc
Gabriel Victor <gabrielvictorxd@gmail.com>
2024-08-17 20:15:16 -0300

    Correção do README

--- script.sql
+++ script.sql
@@ -1,7 +1,6 @@
 use Banco_dados;
 
--- Vamos adicionar essa a coluna CodigoCurso ao Ranking
--- Como o ranking é por Série e Curso, achei muito pertinente ele estár na tabela ranking
+-- Já que a tabela Ranking é por Série e Curso, achei muito pertinente adicionar a coluna CodigoCurso.
 ALTER TABLE Ranking ADD CodigoCurso INT NOT NULL;
 
 
@@ -18,7 +17,7 @@ BEGIN
 
         WITH MediaBolsaMerito AS (
             SELECT
-                -- Esse Case Altera de onde a nota virá para a sua base de cálculo, Notas.Media1 ou Notas.MediaFinal, com base no Semestre 1 e 2 respectivamente
+                -- Esse CASE altera de onde a nota virá - Notas.Media1 ou Notas.MediaFinal - para a sua base de cálculo, com base no Semestre 1 e 2 respectivamente
                 Notas.CH * ((CASE WHEN Notas.Semestre = 1 THEN Notas.Media1 WHEN Notas.Semestre = 2 THEN Notas.MediaFinal ELSE NULL END))
                 -- Nessa parte, eu retorno a soma da carga horária das matérias que o aluno está cursando,
                 -- respeitando a regra de Disciplinas que não entram no calculo de acordo com o semestre
@@ -79,17 +78,17 @@ BEGIN
             MediaBolsaMerito.Turma,
             MediaBolsaMerito.CodigoCurso,
             count(*) as QtdeDisciplina,
-            -- usei o round, mas não foi especificado o tipo de arredondamento usado como floor ou ceiling
+            -- Usei o ROUND por não ter sido especificado o tipo de arredondamento usado, como FLOOR ou CEILING.
             ROUND(SUM(MediaBolsaMerito.MediaSemestral),3) as MediaBolsaMerito,
-            -- O dense_rank da ranking aos registros ordenado pela média bolsa merito, cada curso e série do curso tem seu próprio ranking,
-            -- quando a nota for igual o ranking será o mesmo
+            -- O DENSE_RANK ordena os registros pela MediaBolsaMerito, considerando que cada curso e série tem seu próprio ranking.
+            -- Quando a nota for igual, o ranking será o mesmo.
             DENSE_RANK() OVER (
-                -- Adicionei esse LEFT, pois na documentação é informado que o semestre é informado pelo primeiro caracter da turma
+                -- Adicionei esse LEFT pois na documentação é informado que o semestre é informado pelo primeiro caracter da turma
                 PARTITION BY LEFT(MediaBolsaMerito.Turma, 1), MediaBolsaMerito.CodigoCurso
                 ORDER BY ROUND(SUM(MediaBolsaMerito.MediaSemestral),3) desc
             ) as PosicaoBolsaMerito,
-            -- primeiramente coloquei a nota 0-10 e dei um arredondamento com duas casas decimais.
-            -- Regra: se o aluno ficar com a média entre 7,75 e 7,99 a nota deve ser arredondada para 8
+            -- Primeiramente, coloquei a nota de 0-10 e apliquei um arredondamento com duas casas decimais.
+            -- REGRA: se o aluno ficar com a média entre 7.75 e 7.99, a nota deve ser arredondada para 8.
             CASE
                 WHEN ROUND(SUM(MediaBolsaMerito.MediaSemestral)/10,2) >= 7.75 AND ROUND(SUM(MediaBolsaMerito.MediaSemestral)/10,2) < 8 THEN
                 8
@@ -106,7 +105,7 @@ BEGIN
             MediaBolsaMerito.CodigoCurso;
         -- Fiz separado a distribuição das bolsas para ficar menos complexo a Query
         -- Toda distribuição leva em conta o ranking do curso e semestre. Se tiver
-        -- um aluno com a mesma média a porcentagem da bolsa será dividida entre eles.
+        -- um aluno com a mesma média, a porcentagem da bolsa será dividida entre eles.
         UPDATE
             Ranking
         SET PorcentagemBolsaMerito = ROUND(@porcentagem_bolsa_primeiro / (
@@ -154,10 +153,9 @@ BEGIN
     END CATCH
 END;
 
--- Pensando no longo prazo, vamos criar alguns indices
+-- Pensando na consulta das tabelas a longo prazo, vamos criar alguns índices para manter o alto desempenho:
 
--- Escolhi o DESC pois acredito que o sistema usará frequentemente dados mais recentes em suas funcionalidades
--- O que acredito que a longo prazo, manterá o alto desempenho
+-- Escolhi o DESC, pois acredito que o sistema usará frequentemente dados mais recentes em suas consultas e funcionalidades.
 
 -- Index RM
 CREATE INDEX idx_rm_notas ON Notas (RM DESC);
@@ -170,12 +168,12 @@ CREATE INDEX idx_codigo_curso_ranking ON Ranking (CodigoCurso DESC);
 -- Index CodigoDisciplina
 CREATE INDEX idx_codigo_disciplina_notas ON Notas (CodigoDisciplina DESC);
 
--- Index Turma - Coloquei ASC só para manter o padrão, pois não fará diferença para esse campo o DESC
+-- Index Turma: coloquei ASC só para manter o padrão, pois não fará diferença para esse campo o DESC
 CREATE INDEX idx_turma_notas ON Notas (Turma ASC);
 CREATE INDEX idx_turma_ranking ON Ranking (Turma ASC);
 
--- Acredito que há mais index para ser criado, mas inicialmente para o nosso propósito está ok.
--- Devemos tomar cuidado na criação de muitos indices, pois invés de ajudar pode atrapalhar, causando lentidão que é a ideia contrária dos índices.
+-- Acredito que há mais índexes para serem criados, mas inicialmente para o nosso propósito está ok.
+-- Devemos tomar cuidado na criação de muitos índices, pois, ao invés de ajudar, pode atrapalhar causando lentidão, que é a ideia contrária dos índices.
 
 -- Vamos executar a procedure
 EXEC InserirRankingMerito;
